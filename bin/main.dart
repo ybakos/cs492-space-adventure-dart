00db4244bcaa77cfa09b41ab71b0d2b25461eacc
Yong Joseph Bakos <ybakos@humanoriented.com>
2019-07-26 15:23:08 -0700

    Main: Print the text we expect to see once complete.
    main.
    Paste in the text, wrap them in print calls.
    Note the use of escaping single quotes.
    String literals, calling functions.

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,16 @@
 import 'package:dart_space_adventure/dart_space_adventure.dart' as dart_space_adventure;
 
 main(List<String> arguments) {
-  print('Hello world: ${dart_space_adventure.calculate()}!');
+  print('Welcome to the Solar System!');
+  print('There are 8 planets to explore.');
+  print('What is your name?');
+  print('Jane');
+  print('Nice to meet you, Jane. My name is Eliza, I\'m an old friend of Alexa.');
+  print('Let\'s go on an adventure!');
+  print('Shall I randomly choose a planet for you to visit? (Y or N)');
+  print('N');
+  print('Name the planet you would like to visit.');
+  print('Neptune');
+  print('Traveling to Neptune...');
+  print('Arrived at Neptune. A very cold planet, furthest from the sun.');
 }
