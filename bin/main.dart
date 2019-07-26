4bea4b019555c2533dfa6f23fa3a2d1e2c9f8aca
Yong Joseph Bakos <ybakos@humanoriented.com>
2019-07-26 15:47:51 -0700

    Main: Capture user input and echo back.
    String interpolation, variables, import, stdin, multi-line string literals,
    newline escape code.

--- bin/main.dart
+++ bin/main.dart
@@ -1,16 +1,29 @@
 import 'package:dart_space_adventure/dart_space_adventure.dart' as dart_space_adventure;
+import 'dart:io';
 
 main(List<String> arguments) {
   print('Welcome to the Solar System!');
   print('There are 8 planets to explore.');
+  
   print('What is your name?');
-  print('Jane');
-  print('Nice to meet you, Jane. My name is Eliza, I\'m an old friend of Alexa.');
-  print('Let\'s go on an adventure!');
-  print('Shall I randomly choose a planet for you to visit? (Y or N)');
-  print('N');
+
+  String name = stdin.readLineSync();
+
+  print('Nice to meet you, $name. My name is Eliza, I\'m an old friend of Alexa.');
+
+  print(
+    'Let\'s go on an adventure!\n'
+    'Shall I randomly choose a planet for you to visit? (Y or N)'
+  );
+
+  String answer = stdin.readLineSync();
+
   print('Name the planet you would like to visit.');
-  print('Neptune');
-  print('Traveling to Neptune...');
-  print('Arrived at Neptune. A very cold planet, furthest from the sun.');
+
+  String planetName = stdin.readLineSync();
+
+  print(
+    'Traveling to $planetName...\n'
+    'Arrived at $planetName. A very cold planet, furthest from the sun.'
+  );
 }
