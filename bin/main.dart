fcb0f6ad1c6119b0a80f06d68a858871a54430e1
Yong Joseph Bakos <ybakos@humanoriented.com>
2019-07-27 08:39:11 -0700

    Main: Extract into functions.

--- bin/main.dart
+++ bin/main.dart
@@ -2,37 +2,62 @@ import 'package:dart_space_adventure/dart_space_adventure.dart' as dart_space_ad
 import 'dart:io';
 
 main(List<String> arguments) {
+  printGreeting();
+  printIntroduction(responseToPrompt('What is your name?'));
+  print('Let\'s go on an adventure!');
+  travel(
+    promptForRandomOrSpecificDestination(
+      'Shall I randomly choose a planet for you to visit? (Y or N)'
+    )
+  );
+}
+
+void printGreeting() {
   print('Welcome to the Solar System!');
   print('There are 8 planets to explore.');
-  
-  print('What is your name?');
+}
 
-  final name = stdin.readLineSync();
+String responseToPrompt(String prompt) {
+  print(prompt);
+  return stdin.readLineSync();
+}
 
+void printIntroduction(String name) {
   print('Nice to meet you, $name. My name is Eliza, I\'m an old friend of Alexa.');
+}
 
-  print(
-    'Let\'s go on an adventure!\n'
-    'Shall I randomly choose a planet for you to visit? (Y or N)'
-  );
-
+bool promptForRandomOrSpecificDestination(String prompt) {
   String answer;
   while (answer != 'Y' && answer != 'N') {
-    answer = stdin.readLineSync();
+    answer = responseToPrompt(prompt);
     if (answer == 'Y') {
-      print(
-        'Ok! Traveling to Mercury...\n'
-        'Arrived at Mercury. A very hot planet, closest to the sun.'
-      );
+      return true;
     } else if (answer == 'N') {
-      print('Name the planet you would like to visit.');
-      final planetName = stdin.readLineSync();
-      print(
-        'Traveling to $planetName...\n'
-        'Arrived at $planetName. A very cold planet, furthest from the sun.'
-      );
+      return false;
     } else {
       print('Sorry, I didn\'t get that.');
     }
   }
+}
+
+void travel(bool randomDestination) {
+  if (randomDestination) {
+    travelToRandomDestination();
+  } else {
+    travelTo(responseToPrompt('Name the planet you would like to visit.'));
+  }
+}
+
+void travelToRandomDestination() {
+  print(
+    'Ok! Traveling to Mercury...\n'
+    'Arrived at Mercury. A very hot planet, closest to the sun.'
+  );
+}
+
+void travelTo(String destination) {
+  print(
+    'Traveling to $destination...\n'
+    'Arrived at $destination. A very cold planet, furthest from the sun.'
+  );
 }
